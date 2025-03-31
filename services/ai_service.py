206ae111340117342ba2dafd2f1b36ddc4c83943
Xbyit <u.d.m.mca@gmail.com>
2025-03-31 11:58:22 +0100

    Update ai_service.py

--- services/ai_service.py
+++ services/ai_service.py
@@ -3,7 +3,7 @@ import requests
 
 def web_ai(urls,query):
     try :
-        response=requests.get(f"https://dev-pycodz-blackbox.pantheonsite.io/DEvZ44d/deepseek.php?text=I will send you a set of linksin json format, arrange them using the search feature based on the following topic (query) If you don't find a topic, make your own conclusion the urls {urls} the result shuld be in json format")
+        response=requests.get(f"https://dev-pycodz-blackbox.pantheonsite.io/DEvZ44d/deepseek.php?text=I will send you a set of linksin json format, arrange them using the search feature based on the following topic {query} If you don't find a topic, make your own conclusion the urls {urls} the result shuld be in json format")
         response.raise_for_status()  # Raise an error for bad responses
         # Assuming the response is JSON, return the JSON data
         return response.json()  # This will return the JSON content of the response
